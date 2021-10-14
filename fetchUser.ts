06df9ec41edc42689e600e0c40993fc330015b2d
spiderq2697 <spiderq10@gmail.com>
2021-10-14 18:32:16 +0900

    jest

---
+++ fetchUser.ts
@@ -0,0 +1,13 @@
+function fetchUser(id: number, cb) {
+    setTimeout(() => {
+        console.log("wait 0.1 sec.");
+        const user = {
+            id: id,
+            name: "User" + id,
+            email: id + "@test.com",
+        };
+        cb(user);
+    }, 100);
+}
+
+module.exports = fetchUser;
